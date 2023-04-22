c6055975edf6704d5f851f9d02231402bc8a7c7d
gaoachao <1227448581@qq.com>
2023-04-22 23:05:39 +0000

    update 4.22

--- tcp/client.go
+++ tcp/client.go
@@ -1,15 +1,33 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
+	"os"
+	"strings"
 )
 
-func main(){
-	conn,err := net.Dial("tcp","127.0.0.1:8888")
+func main() {
+	conn, err := net.Dial("tcp", "127.0.0.1:8888")
 	if err != nil {
-		fmt.Println("client dial err =",err)
+		fmt.Println("client dial err =", err)
 		return
 	}
-	fmt.Println("client dial conn =",conn)
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("readString err =", err)
+		}
+		line = strings.Trim(line," \r\n")
+		if line == "exit" {
+			fmt.Println("当前客户端已退出") 
+			break
+		}
+		_, err = conn.Write([]byte(line + "\n"))
+		if err != nil {
+			fmt.Println("conn.Write err =", err)
+		}
+	}
 }
--- tcp/server.go
+++ tcp/server.go
@@ -1,9 +1,30 @@
 package main
 
-import(
+import (
 	"fmt"
+	"io"
 	"net"
 )
+func process(conn net.Conn){
+	defer conn.Close()
+	// 循环接收某个客户端发送的数据
+	for {
+		// 创建一个新的切片
+		buf := make([]byte,1024)
+		// 等待客户端通过conn发送信息，如果客户端没有Write，那么协程就会阻塞
+		// fmt.Printf("等待客户端 %v 发送数据 \n",conn.RemoteAddr().String())
+		n,err := conn.Read(buf)
+		if err == io.EOF {
+			fmt.Printf("客户端 %v 已退出\n",conn.RemoteAddr().String())
+			return
+		}	else if err != nil {
+			fmt.Println("客户端出现错误",err)
+			return
+		}
+		// 显示客户端发送的内容在服务端的终端
+		fmt.Print(string(buf[:n]))
+	}
+}
 
 func main(){
 	fmt.Println("服务器开始监听...")
@@ -21,9 +42,9 @@ func main(){
 			fmt.Println("Accept() err=",err)
 		} else {
 			// conn.RemoteAddr()表示客户端的接口
-			fmt.Println("Accept() conn=",conn,conn.RemoteAddr().String())
+			fmt.Printf("客户端 %v 已连接 \n",conn.RemoteAddr().String())
 		}
-
+		go process(conn)
 	}
 
 	// fmt.Println("listen = ",listen)
